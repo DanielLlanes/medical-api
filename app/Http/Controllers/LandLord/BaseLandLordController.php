e300cc8e419b68770c56f6031c552507b5c84486
Gabriel <gabriel@example.com>
2026-01-08 20:39:06 -0800

    estructura: carpetas de database en minusculas y namespaces definidos

---
+++ app/Http/Controllers/LandLord/BaseLandLordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\LandLord;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseLandLordController extends Controller
+{
+    
+}
